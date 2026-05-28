a4b90df10ee23b7f3ff4f879ac7d4b2cdfc84c05
Matthew Ceko <matthew.ceko@petermac.org>
2026-05-28 16:36:59 +1000

    unified cli

--- tanpopo/clustering.py
+++ tanpopo/clustering.py
@@ -1,5 +1,3 @@
-import argparse
-
 import anndata as ad
 import pandas as pd
 import scanpy as sc
@@ -9,65 +7,8 @@ from tanpopo.plot import plot_gene_clusters, plot_umap
 from tanpopo.utils import timed, argtop
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Spatial eigenmode based clustering")
-    parser.add_argument(
-        "-i",
-        "--input",
-        type=str,
-        required=True,
-        help="Input .h5ad-formatted hdf5 file with spatial basis information",
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        help="Save .h5ad-formatted hdf5 file",
-    )
-    parser.add_argument(
-        "--neighbours",
-        type=int,
-        default=15,
-        help="Number of neighbours",
-    )
-    parser.add_argument(
-        "--resolution",
-        type=float,
-        default=1.0,
-        help="Leiden resolution",
-    )
-    parser.add_argument(
-        "--metric",
-        type=str,
-        default="cosine",
-        help="Clustering metric",
-    )
-    parser.add_argument(
-        "--ngenes",
-        type=int,
-        help="Filter top n genes",
-    )
-    parser.add_argument(
-        "--plot",
-        action="store_true",
-        help="Plot spatial gene basis",
-    )
-    parser.add_argument(
-        "--umap",
-        action="store_true",
-        help="Compute and plot umap",
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        help="Time and print each step",
-    )
-    return parser.parse_args()
-
-
-def setup(mode):
-    """Parse input"""
-    args = parse_args()
+def cluster_cmd(args):
+    """Cluster spots or genes based on eigenmodes"""
     with timed("Loading data", args.verbose):
         adata = sc.read_h5ad(args.input)
 
@@ -80,13 +21,16 @@ def setup(mode):
     if adata.uns["tanpopo"].get("clustering") is None:
         adata.uns["tanpopo"]["clustering"] = {}
 
-    adata.uns["tanpopo"]["clustering"][mode] = {
+    adata.uns["tanpopo"]["clustering"][args.by] = {
         "n_neighbours": args.neighbours,
         "resolution": args.resolution,
         "metric": args.metric,
     }
 
-    return args, adata
+    if args.by == "spots":
+        cluster_spots(args, adata)
+    else:
+        cluster_genes(args, adata)
 
 
 def cluster_leiden(X, n_neighbors=15, resolution=1.0, metric="cosine", random_state=0):
@@ -107,9 +51,8 @@ def cluster_leiden(X, n_neighbors=15, resolution=1.0, metric="cosine", random_st
     return pd.Categorical(labels, ordered=True)
 
 
-def cluster_spots():
+def cluster_spots(args, adata):
     """Cluster spots based on spatial eigenmodes"""
-    args, adata = setup("spots")
 
     with timed("Spot clustering", args.verbose):
         adata.obs["tanpopo_leiden"] = cluster_leiden(
@@ -143,9 +86,8 @@ def cluster_spots():
         adata.write(args.output)
 
 
-def cluster_genes():
+def cluster_genes(args, adata):
     """Cluster genes based on eigendecomposition gene scores"""
-    args, adata = setup("genes")
 
     with timed("Gene clustering", args.verbose):
         adata.var["tanpopo_leiden"] = cluster_leiden(
